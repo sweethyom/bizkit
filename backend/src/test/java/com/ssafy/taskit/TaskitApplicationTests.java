6ca3f62742e762ef5d2047d61f5c90fb88ca1092
김덕윤 <duk9741@gmail.com>
2025-04-24 11:02:02 +0900

    chore: init backend project

---
+++ backend/src/test/java/com/ssafy/taskit/TaskitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.taskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
