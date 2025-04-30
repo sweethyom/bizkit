4aae5debc9c35192cbf17c656900dcdd9d3576f8
이효미 <smilelhm@naver.com>
2025-04-30 15:44:27 +0900

    docs: 나의 프로젝트 목록 조회 API 문서화

--- backend/core/core-api/src/test/java/com/ssafy/taskit/api/controller/ProjectControllerTest.java
+++ backend/core/core-api/src/test/java/com/ssafy/taskit/api/controller/ProjectControllerTest.java
@@ -2,6 +2,8 @@ package com.ssafy.taskit.api.controller;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 
 import com.ssafy.s12p21d206.achu.test.api.RestDocsTest;
 import com.ssafy.s12p21d206.achu.test.api.RestDocsUtils;
@@ -37,7 +39,35 @@ class ProjectControllerTest extends RestDocsTest {
             responseFields(
                 fieldWithPath("result")
                     .type(JsonFieldType.STRING)
-                    .description("성공 여부 (예: SUCCESS 혹은 ERROR"),
+                    .description("성공 여부 (예: SUCCESS 혹은 ERROR)"),
                 fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("생성된 프로젝트의 id"))));
+  }
+
+  @Test
+  public void findProjects() {
+    given()
+        .contentType(ContentType.JSON)
+        .get("/projects")
+        .then()
+        .status(HttpStatus.OK)
+        .apply(document(
+            "find-projects",
+            queryParameters(parameterWithName("cursor")
+                .optional()
+                .description("이전 페이지의 마지막 프로젝트 ID(커서, 첫 페이지 요청 시 생략)")),
+            responseFields(
+                fieldWithPath("result")
+                    .type(JsonFieldType.STRING)
+                    .description("성공 여부 (예: SUCCESS 혹은 ERROR)"),
+                fieldWithPath("data.[].id").type(JsonFieldType.NUMBER).description("내가 속한 프로젝트 id"),
+                fieldWithPath("data.[].name")
+                    .type(JsonFieldType.STRING)
+                    .description("내가 속한 프로젝트 이름"),
+                fieldWithPath("data.[].image")
+                    .type(JsonFieldType.STRING)
+                    .description("내가 속한 프로젝트 이미지 경로"),
+                fieldWithPath("data.[].count")
+                    .type(JsonFieldType.NUMBER)
+                    .description("프로젝트 내 나의 할 일 개수"))));
   }
 }
