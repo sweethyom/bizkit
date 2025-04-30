747aba4289abb2c854dd4b29a085dc280591a109
최재영 <horizontal4@naver.com>
2025-04-30 13:46:11 +0900

    docs: 컴포컴컴포넌트 ì 생성 API 문서화

--- backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/ComponentController.java
+++ backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/ComponentController.java
@@ -3,15 +3,15 @@ package com.ssafy.taskit.api.controller;
 import com.ssafy.taskit.api.response.ApiResponse;
 import com.ssafy.taskit.api.response.DefaultIdResponse;
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComponentController {
 
-    @PostMapping("/projects/{projectId}/components")
-    public ApiResponse<DefaultIdResponse> appendComponent(
-            ApiUser apiUser, @PathVariable Long projectId, @RequestBody AppendComponentRequest request) {
-        return ApiResponse.success(new DefaultIdResponse(1L));
-    }
+  @PostMapping("/projects/{projectId}/component")
+  public ApiResponse<DefaultIdResponse> appendComponent(
+      ApiUser apiUser, @PathVariable Long projectId, @RequestBody AppendComponentRequest request) {
+    DefaultIdResponse response = new DefaultIdResponse(1L);
+    return ApiResponse.success(response);
+  }
 }
---
+++ backend/core/core-api/src/test/java/com/ssafy/taskit/api/controller/ComponentControllerTest.java
@@ -0,0 +1,55 @@
+package com.ssafy.taskit.api.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
+
+import com.ssafy.s12p21d206.achu.test.api.RestDocsTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+class ComponentControllerTest extends RestDocsTest {
+
+  private ComponentController controller;
+
+  @BeforeEach
+  void setup() {
+    controller = new ComponentController();
+    mockMvc = mockController(controller);
+  }
+
+  @Test
+  void appendComponent() {
+    given()
+        .contentType(ContentType.JSON)
+        .body(new AppendComponentRequest("컴포넌트 아이디", "컴포넌트 설명"))
+        .post("/projects/{projectId}/component", 1L)
+        .then()
+        .status(HttpStatus.OK)
+        .apply(document(
+            "append-component",
+            pathParameters(parameterWithName("projectId").description("컴포넌트를 생성하고 싶은 프로젝트 아이디")),
+            requestFields(
+                fieldWithPath("name")
+                    .type(JsonFieldType.STRING)
+                    .description("등록할 컴포넌트의 이름")
+                    .attributes(key("constraints").value("최대 20bytes (앞, 뒤 공백 불가)")),
+                fieldWithPath("description")
+                    .optional()
+                    .type(JsonFieldType.STRING)
+                    .description("등록할 컴포넌트의 설명")
+                    .attributes(key("constraints").value("최대 100bytes (앞, 뒤 공백 불가)"))),
+            responseFields(
+                fieldWithPath("result")
+                    .type(JsonFieldType.STRING)
+                    .description("성공 여부 (예: SUCCESS 혹은 ERROR"),
+                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("생성된 컴포넌트 아이디"))));
+  }
+}
