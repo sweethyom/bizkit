14c63a76aa06220fb7270f9ca46f1bd2bd975d04
leehyomi <smilelhm7@gmail.com>
2025-05-01 03:05:58 +0900

    feat: 초대 아이디로 프로젝트 정보 조회 목 API 구현

--- backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/ProjectController.java
+++ backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/ProjectController.java
@@ -47,9 +47,8 @@ public class ProjectController {
     return ApiResponse.success();
   }
 
-  @GetMapping("/projects/{projectId}/invitation")
-  public ApiResponse<InvitationProjectResponse> findInvitationProject(
-      @PathVariable Long projectId) {
+  @GetMapping("/projects/invitation/{invitationId}")
+  public ApiResponse<InvitationProjectResponse> findInvitationProject(@PathVariable Long invitationId) {
     LeaderResponse leaderResponse = new LeaderResponse(1L, "팀장1", "profile1.jpg");
     InvitationProjectResponse response =
         new InvitationProjectResponse(1L, "프로젝트 이름1", "default1.jpg", leaderResponse);
