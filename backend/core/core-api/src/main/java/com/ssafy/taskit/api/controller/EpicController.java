b59f79ea49571c70d015ba6e242a23c67b3e95a7
채용수 <chaeyong0320@naver.com>
2025-05-08 12:55:10 +0900

    feat: 에픽 목록 조회 API 구현

--- backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/EpicController.java
+++ backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/EpicController.java
@@ -4,8 +4,10 @@ import com.ssafy.taskit.api.response.ApiResponse;
 import com.ssafy.taskit.api.response.DefaultIdResponse;
 import com.ssafy.taskit.domain.Epic;
 import com.ssafy.taskit.domain.EpicService;
+import com.ssafy.taskit.domain.IssueService;
 import com.ssafy.taskit.domain.NewEpic;
 import java.util.List;
+import java.util.Map;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EpicController {
 
   private final EpicService epicService;
+  private final IssueService issueService;
 
-  public EpicController(EpicService epicService) {
+  public EpicController(EpicService epicService, IssueService issueService) {
     this.epicService = epicService;
+    this.issueService = issueService;
   }
 
   @PostMapping("projects/{projectId}/epics")
@@ -36,9 +40,19 @@ public class EpicController {
 
   @GetMapping("projects/{projectId}/epics")
   public ApiResponse<List<EpicResponse>> findEpics(ApiUser apiUser, @PathVariable Long projectId) {
-    List<EpicResponse> response = List.of(
-        new EpicResponse(1L, "S12P31D207-1", "에픽1", 10L, 3L),
-        new EpicResponse(2L, "S12P31D207-2", "에픽2", 20L, 17L));
+    List<Epic> epics = epicService.findEpics(apiUser.toUser(), projectId);
+    List<Long> epicIds = epics.stream().map(Epic::id).toList();
+    // 에픽 서비스가 아니라 이슈 서비스에서 하거나 cntService 따로 만들어야 할 듯?->이슈에서 해야할 듯
+    // 에픽에서 보는게 아니고 이슈에서
+    // 해당 이슈의 epicId가 epicIds에 있고(WHERE i.epicId in :epicIds)
+    // 엔티티상태 active인(and i.entitystatus = :status(active)) 이슈 갯수
+    // 전체이슈갯수 : select i.epicId, count(i) from IssueEntity i where i.epicId in :epicIds and
+    // i.entitystatus = :status group by i.epicId
+    // 남은 이슈 갯수는 where에 i.issuestatus not in :Done 하면 될 듯? -> 아님 스프린트 미할당인 이슈 갯수임
+    Map<Long, Integer> totalIssueCountMap = issueService.countTotalIssues(epicIds);
+    Map<Long, Integer> backlogIssueCountMap = issueService.countBacklogIssues(epicIds);
+    List<EpicResponse> response =
+        EpicResponse.from(epics, totalIssueCountMap, backlogIssueCountMap);
     return ApiResponse.success(response);
   }
 
