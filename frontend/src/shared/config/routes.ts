93c37d561a37c2c171ed0961525601a1c470f1bc
전준표 <junpyo0920@gmail.com>
2025-05-08 14:43:40 +0900

    Revert "Merge branch 'feature/S12P31D207-141' into 'frontend'"
    This reverts merge request !15

--- frontend/src/shared/config/routes.ts
+++ frontend/src/shared/config/routes.ts
@@ -3,20 +3,4 @@ export const ROUTES_MAP = {
     path: '/',
     name: 'Main',
   },
-  signin: {
-    path: '/signin',
-    name: 'SignIn',
-  },
-  projectSettings: {
-    path: '/projects/:projectId/settings',
-    name: 'ProjectSettings',
-  },
-  teamSettings: {
-    path: '/projects/:projectId/team',
-    name: 'TeamSettings',
-  },
-  componentSettings: {
-    path: '/projects/:projectId/components',
-    name: 'ComponentSettings',
-  },
 };
