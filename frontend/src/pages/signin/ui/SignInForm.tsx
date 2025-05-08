0ce96edf0b9e1806f75350dd90cde8b0242cf8ef
swl <hohnmb@naver.com>
2025-05-08 15:48:01 +0900

    로그인 중 svg 수정

--- frontend/src/pages/signin/ui/SignInForm.tsx
+++ frontend/src/pages/signin/ui/SignInForm.tsx
@@ -1,6 +1,6 @@
 // signin/ui/SignInForm.tsx
 import { SignInCredentials } from '@/pages/signin/model/types';
-import { AlertCircle, Lock, Mail } from 'lucide-react';
+import { AlertCircle, LoaderCircle, Lock, Mail } from 'lucide-react';
 import { FC, FormEvent } from 'react';
 
 interface SignInFormProps {
@@ -112,26 +112,7 @@ const SignInForm: FC<SignInFormProps> = ({
         >
           {isLoading ? (
             <div className='flex items-center'>
-              <svg
-                className='animate-spin -ml-1 mr-2 h-4 w-4 text-white'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-              >
-                <circle
-                  className='opacity-25'
-                  cx='12'
-                  cy='12'
-                  r='10'
-                  stroke='currentColor'
-                  strokeWidth='4'
-                ></circle>
-                <path
-                  className='opacity-75'
-                  fill='currentColor'
-                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                ></path>
-              </svg>
+              <LoaderCircle className='animate-spin -ml-1 mr-2 h-4 w-4 text-white' />
               로그인 중...
             </div>
           ) : (
