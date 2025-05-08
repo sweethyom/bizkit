aa7ed24211919e04e2b674534243709865aece1f
채용수 <chaeyong0320@naver.com>
2025-05-08 14:23:54 +0900

    feat: 에픽 이름 수정 db-core 구현

--- backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/EpicCoreRepository.java
+++ backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/EpicCoreRepository.java
@@ -2,7 +2,10 @@ package com.ssafy.taskit.storage.db.core;
 
 import com.ssafy.taskit.domain.Epic;
 import com.ssafy.taskit.domain.EpicRepository;
+import com.ssafy.taskit.domain.ModifyEpic;
 import com.ssafy.taskit.domain.NewEpic;
+import com.ssafy.taskit.domain.error.CoreErrorType;
+import com.ssafy.taskit.domain.error.CoreException;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
@@ -27,5 +30,27 @@ public class EpicCoreRepository implements EpicRepository {
     List<EpicEntity> epicEntities =
         epicJpaRepository.findByProjectIdAndEntityStatus(projectId, EntityStatus.ACTIVE);
     return epicEntities.stream().map(EpicEntity::toEpic).toList();
+  }
+
+  @Override
+  public boolean existsById(Long epicId) {
+    return epicJpaRepository.existsByIdAndEntityStatus(epicId, EntityStatus.ACTIVE);
+  }
+
+  @Override
+  public Epic findById(Long epicId) {
+    return epicJpaRepository
+        .findByEpicIdAndEntityStatus(epicId, EntityStatus.ACTIVE)
+        .orElseThrow(() -> new CoreException(CoreErrorType.DATA_NOT_FOUND))
+        .toEpic();
+  }
+
+  @Override
+  public void modifyEpic(Long epicId, ModifyEpic modifyEpic) {
+    EpicEntity epicEntity = epicJpaRepository
+        .findByEpicIdAndEntityStatus(epicId, EntityStatus.ACTIVE)
+        .orElseThrow(() -> new CoreException(CoreErrorType.DATA_NOT_FOUND));
+    epicEntity.updateEpicName(modifyEpic.name());
+    epicJpaRepository.save(epicEntity);
   }
 }
