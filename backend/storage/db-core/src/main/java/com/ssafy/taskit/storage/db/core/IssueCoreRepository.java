d47fba96490a808824f31ec1fb06fc8785576ac4
채용수 <chaeyong0320@naver.com>
2025-05-09 16:27:56 +0900

    feat: 에픽 하위 이슈 목록 조회 db 구현

--- backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/IssueCoreRepository.java
+++ backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/IssueCoreRepository.java
@@ -2,7 +2,9 @@ package com.ssafy.taskit.storage.db.core;
 
 import com.ssafy.taskit.domain.Issue;
 import com.ssafy.taskit.domain.IssueRepository;
+import com.ssafy.taskit.domain.IssueStatus;
 import com.ssafy.taskit.domain.NewIssue;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,5 +21,12 @@ public class IssueCoreRepository implements IssueRepository {
     return issueJpaRepository
         .save(new IssueEntity(newIssue.name(), key, epicId))
         .toIssue();
+  }
+
+  @Override
+  public List<Issue> findEpicIssues(Long epicId) {
+    List<IssueEntity> issueEntities = issueJpaRepository.findAllByEpicIdAndEntityStatus(
+        epicId, EntityStatus.ACTIVE, IssueStatus.UNASSIGNED);
+    return issueEntities.stream().map(IssueEntity::toIssue).toList();
   }
 }
