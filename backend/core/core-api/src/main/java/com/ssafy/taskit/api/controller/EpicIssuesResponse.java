28e3a8e0fe0604b0aeb36dc74bd3975f09bb94b1
채용수 <chaeyong0320@naver.com>
2025-05-09 16:26:49 +0900

    feat: 에픽 하위 이슈 목록 조회 api 구현

--- backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/EpicIssuesResponse.java
+++ backend/core/core-api/src/main/java/com/ssafy/taskit/api/controller/EpicIssuesResponse.java
@@ -1,7 +1,12 @@
 package com.ssafy.taskit.api.controller;
 
+import com.ssafy.taskit.domain.Assignee;
+import com.ssafy.taskit.domain.Component;
 import com.ssafy.taskit.domain.Importance;
+import com.ssafy.taskit.domain.Issue;
 import com.ssafy.taskit.domain.IssueStatus;
+import java.util.List;
+import java.util.Map;
 
 public record EpicIssuesResponse(
     Long id,
@@ -11,4 +16,27 @@ public record EpicIssuesResponse(
     Importance issueImportance,
     IssueStatus issueStatus,
     ComponentResponse component,
-    AssigneeResponse assignee) {}
+    AssigneeResponse assignee) {
+
+  public static List<EpicIssuesResponse> of(
+      List<Issue> issues, Map<Long, Component> componentMap, Map<Long, Assignee> assigneeMap) {
+    return issues.stream()
+        .map(issue -> {
+          Component component =
+              componentMap.getOrDefault(issue.componentId(), new Component(0L, 0L, 0L, "", ""));
+          Assignee assignee =
+              assigneeMap.getOrDefault(issue.assigneeId(), new Assignee(0L, "", ""));
+
+          return new EpicIssuesResponse(
+              issue.id(),
+              issue.name(),
+              issue.key(),
+              issue.bizPoint(),
+              issue.issueImportance(),
+              issue.issueStatus(),
+              new ComponentResponse(component.id(), component.name()),
+              new AssigneeResponse(assignee.id(), assignee.nickname(), assignee.profileImageUrl()));
+        })
+        .toList();
+  }
+}
