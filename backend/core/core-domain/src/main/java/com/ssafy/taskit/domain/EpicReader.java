e563e37ce87baf8fd531045ef0de6d28f7958bd6
채용수 <chaeyong0320@naver.com>
2025-05-10 17:38:21 +0900

    feat: 이슈 상세 조회 domain 구현

--- backend/core/core-domain/src/main/java/com/ssafy/taskit/domain/EpicReader.java
+++ backend/core/core-domain/src/main/java/com/ssafy/taskit/domain/EpicReader.java
@@ -9,19 +9,29 @@ public class EpicReader {
   private final ProjectValidator projectValidator;
   private final MemberValidator memberValidator;
   private final EpicRepository epicRepository;
+  private final EpicValidator epicValidator;
 
   public EpicReader(
       ProjectValidator projectValidator,
       MemberValidator memberValidator,
-      EpicRepository epicRepository) {
+      EpicRepository epicRepository,
+      EpicValidator epicValidator) {
     this.projectValidator = projectValidator;
     this.memberValidator = memberValidator;
     this.epicRepository = epicRepository;
+    this.epicValidator = epicValidator;
   }
 
   public List<Epic> readEpics(User user, Long projectId) {
     projectValidator.isProjectExists(projectId);
     memberValidator.isProjectMember(user, projectId);
     return epicRepository.findEpics(projectId);
+  }
+
+  public Epic readEpic(User user, Long epicId) {
+    epicValidator.isEpicExists(epicId);
+    Epic epic = epicRepository.findById(epicId);
+    memberValidator.isProjectMember(user, epic.projectId());
+    return epicRepository.findById(epicId);
   }
 }
