3f88c48926f1289d77bad009d321270f0b4b6a76
채용수 <chaeyong0320@naver.com>
2025-05-10 17:39:17 +0900

    feat: 사용자 관련 db 구현

---
+++ backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/UserEntity.java
@@ -0,0 +1,21 @@
+package com.ssafy.taskit.storage.db.core;
+
+import com.ssafy.taskit.domain.UserDetail;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.Immutable;
+
+@Immutable
+@Table(name = "`user`")
+@Entity
+public class UserEntity extends BaseEntity {
+
+  private String nickname;
+  private String profileImgUrl;
+
+  protected UserEntity() {}
+
+  public UserDetail toUserDetail() {
+    return new UserDetail(this.getId(), this.nickname, this.profileImgUrl);
+  }
+}
---
+++ backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/UserJpaRepository.java
@@ -0,0 +1,5 @@
+package com.ssafy.taskit.storage.db.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {}
