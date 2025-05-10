ff699a0416aa3ade7381752c5a4dd2b501a3770e
채용수 <chaeyong0320@naver.com>
2025-05-10 23:14:00 +0900

    feat: 컴포넌트 별 활성 스프린트 내 이슈 목록 조회 db 구현

---
+++ backend/storage/db-core/src/main/java/com/ssafy/taskit/storage/db/core/SprintEntity.java
@@ -0,0 +1,37 @@
+package com.ssafy.taskit.storage.db.core;
+
+import com.ssafy.taskit.domain.SprintStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Table(name = "sprint")
+@Entity
+public class SprintEntity extends BaseEntity {
+
+  private String name;
+
+  @Enumerated(EnumType.STRING)
+  private SprintStatus sprintStatus;
+
+  private LocalDate startDate;
+
+  private LocalDate dueDate;
+
+  private LocalDate completedDate;
+
+  private Long projectId;
+
+  protected SprintEntity() {}
+
+  public SprintEntity(String name, Long projectId) {
+    this.name = name;
+    this.sprintStatus = SprintStatus.READY;
+    this.startDate = null;
+    this.dueDate = null;
+    this.completedDate = null;
+    this.projectId = projectId;
+  }
+}
